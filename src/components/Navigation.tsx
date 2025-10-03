7e0ed1b7bf564afd42bac0704d491e34ebd8c5a3
B.L.A.C.K.O.P.S <blueyedevelopers@gmail.com>
2025-10-03 16:31:29 +0300

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -22,7 +22,7 @@ const Navigation = () => {
       <div className="container mx-auto px-4">
         <div className="flex items-center justify-between h-16">
           <Link to="/" className="flex items-center gap-2">
-            <img src={logo} alt="PaySift" className="h-12" />
+            <img src={logo} alt="PaySift" className="h-15" />
           </Link>
 
           <div className="hidden md:flex items-center gap-2">
